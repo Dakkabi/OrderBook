06cf50ac72fbc04193f34a9f63c1b4c86e6affeb
Dominic <238093957+Dakkabi@users.noreply.github.com>
2025-10-15 21:26:08 +0100

    #3 Add docstring to Side enum method

--- orderbook/src/main/java/github/dakkabi/engine/model/Side.java
+++ orderbook/src/main/java/github/dakkabi/engine/model/Side.java
@@ -7,6 +7,11 @@ public enum Side {
   BID,
   ASK;
 
+  /**
+   * Get the opposing enum order side.
+   *
+   * @return The opposite Side, e.g. BID -> ASK.
+   */
   public Side other() {
     return this.equals(BID) ? ASK : BID;
   }
