54354b90188598e4ad9ca9991bca5509e4d87a5a
Dominic <238093957+Dakkabi@users.noreply.github.com>
2025-10-15 21:25:30 +0100

    #3 Add onOrderAdded and matchOrder methods, matchOrder is unimplemented

--- orderbook/src/main/java/github/dakkabi/engine/service/MatchingEngine.java
+++ orderbook/src/main/java/github/dakkabi/engine/service/MatchingEngine.java
@@ -1,7 +1,9 @@
 package github.dakkabi.engine.service;
 
 import github.dakkabi.engine.model.MatchingAlgorithm;
+import github.dakkabi.engine.model.Order;
 import github.dakkabi.engine.model.OrderBook;
+import github.dakkabi.engine.model.Type;
 
 /**
  * Service class to handle the matching logic for incoming orders and trades
@@ -34,6 +36,41 @@ public class MatchingEngine {
     }
 
     this.algorithm = algorithm;
+  }
+
+  /**
+   * Attempt to immediately fill the order, before adding to the order book if
+   * available.
+   *
+   * @param order The new order instance.
+   * @return The updated order instance.
+   */
+  public Order onOrderAdded(Order order) {
+    if (matchOrder(order)) {
+      // Order was able to be immediately completed.
+      return order;
+    }
+
+    // Unfilled
+    switch (order.getType()) {
+      case Type.MARKET:
+        // Market Orders get cancelled if not filled.
+        break;
+
+      case Type.LIMIT:
+        // Limit orders are placed onto the Order Book
+        orderBook.addOrder(order);
+        break;
+
+      default:
+        throw new UnsupportedOperationException("Unknown order type: " + order.getType());
+    }
+    return order;
+  }
+
+  private boolean matchOrder(Order order) {
+    // TODO: Implement Matching FIFO
+    return false;
   }
 
   public OrderBook getOrderBook() {
