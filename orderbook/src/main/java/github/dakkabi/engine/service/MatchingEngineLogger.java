955a8dea680c01d2bf44d5d22fc8ec4fe4b2106d
Dominic <238093957+Dakkabi@users.noreply.github.com>
2025-10-15 22:29:41 +0100

    #3 Utilise a string formatter for better clarity, and create logging events for onOrderRemoved

--- orderbook/src/main/java/github/dakkabi/engine/service/MatchingEngineLogger.java
+++ orderbook/src/main/java/github/dakkabi/engine/service/MatchingEngineLogger.java
@@ -1,14 +1,65 @@
 package github.dakkabi.engine.service;
 
 import github.dakkabi.engine.model.Order;
+import github.dakkabi.engine.model.Type;
 
+/**
+ * Observer class to log a Matching Engine's events.
+ */
 public class MatchingEngineLogger {
+  /**
+   * Log Order initialisation.
+   *
+   * @param order The order affected.
+   */
   public void onOrderAdded(Order order) {
-    System.out.println(
-        "[ADD] " + order.getSide() +
-            " Order " + order.getId() +
-            " added: " + order.getRemainingQuantity() +
-            "@" + order.getPrice()
+    String msg = String.format(
+        "[ADD] %s Order %s %d@%.2f",
+        order.getType(),
+        order.getId(),
+        order.getInitialQuantity(),
+        order.getPrice()
     );
+
+    System.out.println(msg);
+  }
+
+  /**
+   * Log when an Order is completed, failed to fill as a market order, or was cancelled by user.
+   *
+   * @param order The order affected.
+   */
+  public void onOrderRemoved(Order order) {
+    String msg;
+    if (order.completed()) {
+      msg = String.format(
+          "[REMOVE] Completed %s Order %s %d@%.2f",
+          order.getType(),
+          order.getId(),
+          order.getInitialQuantity(),
+          order.getPrice()
+      );
+
+    } else if (order.getType() == Type.MARKET) {
+      // Market orders dont get assigned IDs
+      msg = String.format(
+          "[CANCEL] Failed to fill %s Order, %d@%.2f with %s remaining quantity",
+          order.getType(),
+          order.getInitialQuantity(),
+          order.getPrice(),
+          order.getRemainingQuantity()
+      );
+
+    } else { // Most likely user cancelled
+      msg = String.format(
+          "[CANCEL] Cancelled %s Order %s by User, %d@%.2f with %s remaining quantity",
+          order.getType(),
+          order.getId(),
+          order.getInitialQuantity(),
+          order.getPrice(),
+          order.getRemainingQuantity()
+      );
+    }
+    System.out.println(msg);
   }
 }
