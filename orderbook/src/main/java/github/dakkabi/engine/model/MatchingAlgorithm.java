57c6d348f76f4ebce55a279c956847f16b18ddbc
Dominic <238093957+Dakkabi@users.noreply.github.com>
2025-10-15 15:42:05 +0100

    #3 Add docstring to MatchingAlgorithm enum

--- orderbook/src/main/java/github/dakkabi/engine/model/MatchingAlgorithm.java
+++ orderbook/src/main/java/github/dakkabi/engine/model/MatchingAlgorithm.java
@@ -1,6 +1,12 @@
 package github.dakkabi.engine.model;
 
+/**
+ * Enum representing the trading algorithm to utilise for a Matching Engine.
+ *
+ * <p>FIFO: First-in, First-out, uses orders' timestamp to decide priority. <br/>
+ *  PRO-RATA: Uses orders' quantity to decide priority.</p>
+ */
 public enum MatchingAlgorithm {
-  FIFO, // First-in, First-out, use timestamp for priority
-  PRORATA // Use quantity as priority
+  FIFO,
+  PRORATA
 }
