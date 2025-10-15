0056346c8f8c9408555a856ee1eaee7d256c3ddf
Dominic <238093957+Dakkabi@users.noreply.github.com>
2025-10-15 21:49:48 +0100

    #3 Add spaces for string formatting

--- orderbook/src/main/java/github/dakkabi/engine/service/MatchingEngineLogger.java
+++ orderbook/src/main/java/github/dakkabi/engine/service/MatchingEngineLogger.java
@@ -5,8 +5,8 @@ import github.dakkabi.engine.model.Order;
 public class MatchingEngineLogger {
   public void onOrderAdded(Order order) {
     System.out.println(
-        "[ADD]" + order.getSide() +
-            "Order " + order.getId() +
+        "[ADD] " + order.getSide() +
+            " Order " + order.getId() +
             " added: " + order.getRemainingQuantity() +
             "@" + order.getPrice()
     );
