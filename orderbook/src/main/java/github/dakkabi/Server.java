539ffa3735b2f55040344806348ee559cb865362
Dominic <238093957+Dakkabi@users.noreply.github.com>
2025-10-15 21:49:36 +0100

    #3 Create basic CLI for viewing Logger events

---
+++ orderbook/src/main/java/github/dakkabi/Server.java
@@ -0,0 +1,58 @@
+package github.dakkabi;
+
+import github.dakkabi.engine.model.MatchingAlgorithm;
+import github.dakkabi.engine.model.Order;
+import github.dakkabi.engine.model.Side;
+import github.dakkabi.engine.model.Type;
+import github.dakkabi.engine.service.MatchingEngine;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Server {
+  private MatchingEngine matchingEngine = new MatchingEngine(MatchingAlgorithm.FIFO);
+
+  public static void main(String[] args) {
+    Server server = new Server();
+    server.run();
+  }
+
+  public void run() {
+    System.out.println("Server starting");
+    System.out.println("Example: BID LIMIT 500@56.54");
+
+    Thread cliThread = new Thread(this::handleUserInput);
+    cliThread.setDaemon(true);
+    cliThread.start();
+
+    while (true) {
+
+    }
+  }
+
+  private void handleUserInput() {
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+      String line;
+      while((line = reader.readLine()) != null) {
+        processInput(line);
+      }
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
+
+  private void processInput(String cmd) {
+    cmd = cmd.trim().toUpperCase();
+
+    String[] parts =  cmd.split(" ");
+
+    Side side = Side.valueOf(parts[0]);
+    Type type = Type.valueOf(parts[1]);
+
+    String[] quantityPrice = parts[2].split("@");
+    int quantity = Integer.parseInt(quantityPrice[0]);
+    double price = Double.parseDouble(quantityPrice[1]);
+
+    matchingEngine.submitOrder(new Order(side, type, quantity, price));
+  }
+}
